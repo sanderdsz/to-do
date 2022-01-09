56350c54d9355bc6a553e9b2d1f2ab6fac28dfca
sanderdsz <sanderdsz@gmail.com>
2022-01-09 10:56:57 -0300

    all tasks done and tests passed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,36 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      setTasks([
+        ...tasks,
+        {
+          id: Math.floor(Math.random() * 100),
+          title: newTaskTitle,
+          isComplete: false
+        }
+      ]);
+    }
+    console.log(tasks);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const obj = tasks.find(task => task.id === id);
+    if (obj) {
+      if (obj.isComplete === false) {
+        obj.isComplete = true;
+      } else {
+        obj.isComplete = false;
+      }
+    }
+    setTasks([...tasks]);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const index = tasks.findIndex(task => task.id === id);
+    tasks.splice(index, 1);
+    setTasks([...tasks]);
   }
 
   return (
